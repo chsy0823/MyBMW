a8b6dc4247df81a95deff3b79606577eb0aa7f03
Elenore(MBP13) <slidingmouse@naver.com>
2019-03-26 07:46:32 +0900

    map test

--- mybmw/Login.js
+++ mybmw/Login.js
@@ -1,11 +1,21 @@
 import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import MapView from 'react-native-daummap';
+
 
 export default class Login extends Component {
     render() {
         return(
             <View style={styles.container}>
-                <Text> test login view</Text>
+                <MapView
+    initialRegion={{
+        latitude: 36.143099,
+        longitude: 128.392905,
+        zoomLevel: 5,
+    }}
+    mapType={"Standard"}
+    style={{ width: 300, height: 300, }}
+/>
             </View>
         );
     }
